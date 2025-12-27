fdefb99fa5c77ce8d24fb1a67cc264a9495e9af6
Nebiyu Elias <nebiyuelias1@gmail.com>
2025-12-27 15:51:17 +0800

    chore: add keymap to +/- height

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,5 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<A-l>", "<cmd>vertical resize +5<CR>")
 vim.keymap.set("n", "<A-h>", "<cmd>vertical resize -5<CR>")
+vim.keymap.set("n", "<A-j>", "<cmd>resize +5<CR>")
+vim.keymap.set("n", "<A-k>", "<cmd>resize -5<CR>")
